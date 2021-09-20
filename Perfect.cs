6dc47aa1374a814e172af4448cf4b335ca7b03e0
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:07:13 +0530

    [Naveen] Added a LogicalProgram

---
+++ Perfect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram_CS
+{
+    class Perfect
+    {
+        //static variables
+        public static int num;
+        public static int sum = 0;
+        public static int IsPerfect()
+        {
+            //Taking input from the user
+            Console.WriteLine("Enter a numbers:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public static void PerfectNumber()
+        {
+            Perfect.IsPerfect();
+            if (num == sum)
+            {
+                Console.WriteLine("{0} is a perfect number", +num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a perfect number", +num);
+            }
+        }
+    }
+}
---
+++ Prime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram.cs
+{
+    class Prime
+    {
+        public static void PrimeNumber()
+        {
+            int flag = 0;
+            Console.WriteLine("Enter a number to check prime:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("{0} is not a prime number", +num);
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("{0} is a prime number", +num);
+            }
+        }
+    }
+}
