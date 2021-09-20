059c820df32b5a0a1421d28c0d7e05b915787217
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 20:05:19 +0530

    [Naveen] Added a code

---
+++ CouponNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram.cs
+{
+    class CouponNumber
+    {
+        public static void GenerateCoupon()
+        {
+            int i;
+            int randomCount = 0;
+            int ran = 0;
+            Console.WriteLine("Please enter your no of distinct coupon numbers you want to add");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intDistinct = new int[n];
+            for (i = 0; i < n; i++)
+            {
+                intDistinct[i] = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    randomCount++;
+                    Random random = new Random();
+                    ran = random.Next(0, 11);
+                    if (ran == intDistinct[i])
+                    {
+                        Console.WriteLine("Random count:" + randomCount);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram.cs
+{
+    class Fibonacci
+    {
+        public static void FibonacciSeries()
+        {
+            //local variables
+            int count = 10;
+            int res;
+            Console.WriteLine("Enter two numbers:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num1=0;
+            //int num2=1;
+            for (int i = 2; i <= count; i++)
+            {
+                res = num1 + num2;
+                Console.WriteLine("" + res);
+                num1 = num2;
+                num2 = res;
+            }
+        }
+
+
+    }
+}
